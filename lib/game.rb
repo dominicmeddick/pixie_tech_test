dc8641b617066afa41a7bcf78e9712b18f9d0cda
Dominic Meddick <dominicmeddick@gmail.com>
2021-08-26 11:13:26 +0100

    first test passing again because I initialized players within the Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,9 @@
 class Game
 
+  def initialize(player_1, player_2)
+    @players = [player_1, player_2]
+  end
+
   #this method will contain the logic for declaring a winner
   def declare_winner
     "Player wins"
