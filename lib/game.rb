44d970be6b725ee4748585af4ae17b8da8ba2e23
Dominic Meddick <dominicmeddick@gmail.com>
2021-08-26 11:12:40 +0100

    two failing tests because of the doubles being introduced into the game_spec.rb

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,6 @@
 class Game
 
+  #this method will contain the logic for declaring a winner
   def declare_winner
     "Player wins"
   end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,18 @@
 require 'game'
+require 'player'
 
 describe Game do 
 
+  subject(:game) { described_class.new(player_1, player_2) }
+  let(:player_1) { double :player }
+  let(:player_2) { double :player }
+
   it 'declares a winner' do
     expect(subject.declare_winner).to eq "Player wins"
+  end
+
+  it 'the a player can pick a card' do
+    expect(player_1.hand).to eq "10"
   end
 
 end
