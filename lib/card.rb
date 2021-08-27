6c44095f0b55b1e0a3453b29f41b3f42d2ed4e93
Dominic Meddick <dominicmeddick@gmail.com>
2021-08-27 11:34:42 +0100

    added initialized with value and suit values. Test passing

--- lib/card.rb
+++ lib/card.rb
@@ -2,4 +2,11 @@
 
 class Card
 
+  attr_accessor :value, :suit
+
+  def initialize(value, suit)
+    @value = value
+    @suit = suit
+  end
+
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -2,10 +2,12 @@ require 'card'
 
 describe Card do
 
-
   it 'checks that a card has a value and a suit' do
     card = Card.new(5, :spades)
-    expect(card).to have_attributes(value: 5, suit: spades)
+    expect(card.value).to eq 5
+    expect(card.suit).to eq :spades
   end
- 
+
 end
+
+
