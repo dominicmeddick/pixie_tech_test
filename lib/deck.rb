458b9f399c8f25b714fdc53c3e7dfb56425bd6bd
Dominic Meddick <dominicmeddick@gmail.com>
2021-08-27 14:56:40 +0100

    Making a more comprehensive test for the Deck class. Still working out the syntax but confident it works in concept

--- lib/deck.rb
+++ lib/deck.rb
@@ -3,11 +3,20 @@ require_relative 'card'
 class Deck
 
   def initialize
-    @cards = [Card.new(5, :spades)]
+    @cards = [Card.new(5, :spades), Card.new(3, :spades)].shuffle
   end
 
+  def length
+    return @cards.length
+  end
+
+  #this takes the card from the top of the shuffled deck
   def draw
     @cards.pop
+  end
+
+  def shallow_copy_cards
+    @cards.dup
   end
 
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -2,10 +2,47 @@ require 'deck'
 
 describe Deck do
 
-  it 'draws a card' do
-    card = Deck.new.draw
-    expect(card.value).to eq 5
-    expect(card.suit).to eq :spades
+  it 'contains 52 cards' do
+    deck = Deck.new
+    cards = deck.shallow_copy_cards
+
+    card_index = 0 
+    for suit in deck.suit do
+      for value in range (1..13)
+        expect(cards.card_index).to eq value, suit
+        card_index += 1
+      end
+    end
   end
+
+  it 'checks that cards are shuffled' do
+    deck = Deck.new
+    ordered_cards = deck.shallow_copy_cards
+    expect(deck.shuffle).not_to eq ordered_cards
+  end
+
+  # it 'draws a card' do
+  #   card = Deck.new.draw
+  #   expect(card.value).to eq 5
+  #   expect(card.suit).to eq :spades
+  # end
+
+  # it 'shuffles a card' do
+  #   card = Deck.new.draw
+  #   expect(card.value).to eq 5
+  #   expect(card.suit).to eq :spades
+  # end
+
+
+  # Once I call shuffle the cards aren't in the same order as before
+  # I need a method that prints an array with the cards
+  # Call that method after the cards are shuffled
+  # compare the before and after to make sure they're different
+
+  # deck = Deck.new
+  # deck must contain 52 cards in the array
+
+  #
+
   
 end
