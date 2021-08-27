c753c45eff594ee9938acf75aafd2ce3a0162462
Dominic Meddick <dominicmeddick@gmail.com>
2021-08-27 11:40:10 +0100

    Failing test for drawing a card in the Draw class

--- lib/card.rb
+++ lib/card.rb
@@ -1,4 +1,5 @@
 # a single card
+ 
 
 class Card
 
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,5 +1,10 @@
 require 'deck'
 
 describe Deck do
+
+  it 'draws a card' do
+    card = Card.new(5, :spades)
+    expect(card.draw.value).to eq 5
+  end
   
 end
