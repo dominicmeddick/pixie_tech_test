4a12a00fa6fb4a09f1fbd01cec3d987a660404a1
Dominic Meddick <dominicmeddick@gmail.com>
2021-08-28 18:10:43 +0100

    passing test for adding players in game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,22 +1,25 @@
 require_relative 'player'
 
+
 class Game
 
+  attr_accessor :players
+
   def initialize
-    @players = Player.new
+    @players = []
   end
 
   def add_player(player)
     @players << player
   end
 
-  # gives a player a hand/card. Will push out into seperate class as program grows
-  def draw_card
-    @players.each { |player| player.hand << rand(1..13) }
-  end
+  # # gives a player a hand/card. Will push out into seperate class as program grows
+  # def draw_card
+  #   @players.each { |player| player.hand << rand(1..13) }
+  # end
 
-  # this method will contain the logic for declaring a winner
-  def declare_winner
-    @players[0].hand[0] > @players[1].hand[0] ? "Player 1 wins" : "Player 2 wins"
-  end
+  # # this method will contain the logic for declaring a winner
+  # def declare_winner
+  #   @players[0].hand[0] > @players[1].hand[0] ? "Player 1 wins" : "Player 2 wins"
+  # end
 end 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,32 +3,12 @@ require 'player'
 
 describe Game do 
 
-  # it 'declares a winner' do
-  #   game = Game.new
-  #   player = Player.new
-  #   player2 = Player.new
-  #   game.add_player(player)
-  #   game.add_player(player2)
-  #   game.draw_card
-  #   allow(game).to receive(:declare_winner).and_return("Player 1 wins")
-  #   expect(game.declare_winner).to eq "Player 1 wins"
-  # end
-
-  # it 'the a player can pick a card' do
-  #   allow(subject).to receive(:draw_card).and_return(10)
-  #   expect(subject.draw_card).to eq 10
-  # end
-
-  # it 'allows user to add players to the game' do
-  #   player = Player.new
-  #   expect(subject.add_player(player)).to eq [player]
-  # end
-
-  # # not happy with this test. Should expect to see the hand array from the player object to include 10. 
-  # it 'allows the player to draw a card into their hand' do
-  #   player = Player.new
-  #   game = Game.new
-  #   allow(game).to receive(:draw_card).and_return(10)
-  #   expect(game.draw_card).to equal(10)
-  # end
+  it 'adds players' do
+    game = Game.new
+    player = Player.new("Dom")
+    player2 = Player.new("David")
+    game.add_player(player)
+    game.add_player(player2)
+    expect(game.players).not_to be nil
+  end
 end
