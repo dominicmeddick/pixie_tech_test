2de922e6bbbe20da66d4ed048f79d523499bbe1e
Dominic Meddick <dominicmeddick@gmail.com>
2021-08-29 10:08:55 +0100

    user can decide how many cards to add to a game test passing

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,22 +3,22 @@ require 'player'
 
 describe Game do 
 
+  let(:game) { Game.new }
+
   it 'adds players' do
-    game = Game.new
     game.add_player("Dom")
     game.add_player("David")
     expect(game.players).not_to be nil
   end
 
   it ' doesnt allow the user to add more than 52 players' do
-    game = Game.new
     52.times { expect(game.add_player("Dom")).to eq true }
     expect(game.add_player("David")).to eq false
   end
 
-  # it 'decides how many cards to deal' do
-  #   game = Game.new
-  #   number = 5
-  #   expect(game.set_number_of_cards(number)).to eq 5
-  # end
+  it 'decides how many cards to deal' do
+    game.add_player("Dom")
+    expect(game.set_number_of_cards(52)).to eq true
+  end
+
 end
