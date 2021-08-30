9a160b1c4c07f4c3ecd254da55655263e226e51d
Dominic Meddick <dominicmeddick@gmail.com>
2021-08-30 23:35:28 +0100

    Fixed rcard real value method

--- lib/card.rb
+++ lib/card.rb
@@ -4,35 +4,16 @@ class Card
 
   include Comparable
 
-  REAL_VALUE = {
-    :ace => 1, 
-    2 => 2, 
-    3 => 3, 
-    4 => 4, 
-    5 => 5, 
-    6 => 6, 
-    7 => 7, 
-    8 => 8, 
-    9 => 9, 
-    10 => 10, 
-    :jack => 11, 
-    :queen => 12, 
-    :king => 13 
-  }
+  @@card_names = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
 
   def initialize(value, suit)
     @value = value
     @suit = suit
   end
 
-  #determines the numerical value of a card e.g it will see an ace as a 1
-  def real_value
-    REAL_VALUE[value]
-  end
-
   # concerts the value and suits of a single card
   def to_s
-    "the #{value} of #{@suit}"
+    "#{@@card_names[value - 1]} of #{@suit.to_s.capitalize}"
   end
 
   # deteremines which card has a higher value
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -8,17 +8,11 @@ describe Card do
     expect(card.suit).to eq :spades
   end
 
-  it 'checks the real value of a card' do 
-    card = Card.new(:ace, :hearts)
-    expect(card.real_value).to eq 1
-  end
-
   it ' converts the real value and suit of a card to a string' do
-    card = Card.new(:jack, :diamonds)
+    card = Card.new(11, :diamonds)
     card_string = card.to_s
-    expect(card_string).to eq "the jack of diamonds"
+    expect(card_string).to eq "Jack of Diamonds"
   end
-
 end
 
 
